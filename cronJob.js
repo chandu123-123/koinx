d6af7c57c17a28ab0d70ec1794a0bcb2ac8ad147
chandu123-123 <103361757+chandu123-123@users.noreply.github.com>
2024-10-10 19:22:29 +0530

    server update

--- cronJob.js
+++ cronJob.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 // '0 */2 * * *'
 cron.schedule('*/2 * * * *', async () => {
   try {
-    await axios.get('http://localhost:3000/api/crypto');
+    await axios.get(`${process.env.NEXT_PUBLIC_LOCALURL}/api/crypto`);
     console.log('Crypto data fetched and updated');
   } catch (error) {
     console.error('Error in scheduled job:', error);
